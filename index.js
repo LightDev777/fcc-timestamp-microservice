2de9a856926c52c8538cabc3d1e7b49741c0141b
Charmie <charmiequino@gmail.com>
2017-09-02 01:53:20 +0800

    updated

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const monthArray = ["January", "February", "March", "April", "May", "June", "Jul
 
 http.createServer(function(req, res) {
 
-  var objUrl = JSON.stringify(req.url);
-  var objUrlClean = objUrl.replace('/', '');
+  var objUrl = url.parse(req.url, true);//JSON.stringify(req.url);
+  //console.log(objUrl);
+  var objUrlClean = objUrl.pathname;
+  objUrlClean = objUrlClean.replace('/', '');
   var q = eval(objUrlClean);
-  console.log(q);
+  console.log(typeof q);
   var result = {
     "unix": 0,
     "natural": ''
   };
 
-  console.log(isNaN(new Date(decodeURI(q))));
+  console.log(isNaN(new Date(Number(q)*1000)));
   if(typeof q === 'string' && isNaN(new Date(decodeURI(q))) !== true) {
     //result.natural = decodeURI(q);
     var dt1 = new Date(decodeURI(q) + 'GMT');
@@ -25,6 +27,7 @@ http.createServer(function(req, res) {
   } else if(typeof q === 'number' && isNaN(new Date(Number(q)*1000)) !== true) {
     result.unix = Number(q);
     var dt2 = new Date(result.unix*1000);
+    console.log('dt2: ' + dt2);
     var dt2_month = monthArray[dt2.getMonth()];
     var dt2_date = dt2.getDate() < 10 ? '0' + dt2.getDate() : dt2.getDate();
     var dt2_year = dt2.getFullYear();
@@ -34,7 +37,8 @@ http.createServer(function(req, res) {
     result.unix = null;
   }
 
-
+  //console.log(res);
+  //res.write(JSON.stringify(result));
   res.end(JSON.stringify(result));
 
 
